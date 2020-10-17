f57a02632f9cab5a86279cbf717b0458d66ed3ca
LaureneRly <70984470+LaureneRly@users.noreply.github.com>
2020-10-17 19:32:52 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -32,24 +32,53 @@ def contains_doubles(items: list) -> bool:
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    return {}
+       best_student = dict()
+       for key, value in student_grades.items():
+           avg = sum(value) / len(value)
+
+           if len(best_student) == 0 or list(best_student.values())[0] < avg:
+              best_student = {key: avg}
+
+       return best_student
 
 
 def frequence(sentence: str) -> dict:
     # TODO: Afficher les lettres les plus fréquentes
     #       Retourner le tableau de lettres
+    frequency = dict()
 
-    return {}
+    for letter in sentence :
+        frequency[letter] = sentence.count(letter)
+
+    sorted_keys = sorted(frequency, key=frequency.__getitem__, reverse=True)
+
+    for key in sorted_keys:
+        if frequency[key] > 5:
+            print("Le caractère {0} revient {1} fois.".format(key, frequency[key]))
+
+    return frequency
 
 
-def get_recipes():
+def get_recipes(dico):
     # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
-    pass
+    name = input("Entrez le nom de votre recette.\n")
+    ingredients = input("Entrez la liste d'ingrédients de la recette, svp séparer les ingrédients par une virgule.\n")
+    dico[name] = ingredients
+    return dico
+
 
 
 def print_recipe(ingredients) -> None:
     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
-    pass
+    name = input("Entrez le nom d'une recette.\n")
+
+    if name in ingredients:
+        print(ingredients[name])
+    else:
+        print("La recette n'existe pas!\n")
+        print(f"Les recettes existentes sont: {list(ingredients.keys())}")
+        print_recipe(ingredients)
+
 
 
 def main() -> None:
@@ -57,7 +86,7 @@ def main() -> None:
     order()
 
     print(f"On vérifie les anagrammes...")
-    anagrams()
+    print(anagrams())
 
     my_list = [3, 3, 5, 6, 1, 1]
     print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
@@ -70,9 +99,11 @@ def main() -> None:
     frequence(sentence)
 
     print("On enregistre les recettes...")
-    recipes = get_recipes()
+    recipes = get_recipes({})
+    recipes = get_recipes(recipes)
 
     print("On affiche une recette au choix...")
+    print_recipe(recipes)
     print_recipe(recipes)
 
 
